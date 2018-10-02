035af6aa127df61c007216622ee5bda1360d2ff3
diego rivas <diego.rivas@ucsp.edu.pe>
2018-10-02 18:39:06 -0500

    tareas IA

---
+++ graph/main.cpp
@@ -0,0 +1,210 @@
+#define GLUT_DISABLE_ATEXIT_HACK
+#include <windows.h>
+#include <GL/glut.h>
+#include <stdlib.h>
+#define KEY_ESC 27
+#include <stdlib.h>
+#include<time.h>
+#include "graph.cpp"
+#include "Point.h"
+#include "nodeAndEdge.h"
+#define REDIM 25
+
+
+
+CGraph<Point, float> graph;
+vector<Point>ans;
+Point A,B;
+
+using namespace std;
+
+void OnMouseClick(int button, int state, int x, int y)
+{
+    int n_x;
+    int n_y;
+    Point* point;
+    int option;
+
+  if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+  {
+    n_x = x/(REDIM-1);
+    n_y = (600-y)/(REDIM-1);
+    cout<<x/(REDIM-1)<<" , "<<(600-y)/(REDIM-1)<<endl;
+    cout<<"1.- nodo inicio"<<endl;
+    cout<<"2.- nodo fin"<<endl;
+    cout<<"3.- encontrar camino"<<endl;
+    cout<<"4.- borrar nodo"<<endl;
+    cin>>option;
+
+    if(option == 1){
+
+        A.setX(n_x);A.setY(n_y);
+
+    }else if(option == 2){
+
+        B.setX(n_x);B.setY(n_y);
+
+    }else if(option == 3){
+        if(ans.size()> 0){
+            ans.clear();
+        }
+        float distance = graph.pathBetweenNodes(A,B,ans);
+        for(auto i = static_cast<int>(ans.size() - 1); i >= 0; --i ){
+            cout << ans[i].getX() << ", " << ans[i].getY() << " -> ";
+        }
+        cout << "//" << endl;
+        cout << "COSTO DEL RECORRIDO: " << distance << endl;
+
+    }else if(option == 4){
+
+        for(int i = 0 ; i < graph.getNodesList().size() ;i++){
+            point = &(graph.getNodesList())[i]->m_data;
+            if ( (*point).getX() == n_x && (*point).getY() == n_y){
+                graph.removeNode(*point);
+                //graph.removeNode((graph.getNodesList())[i]);
+            }
+
+        }
+    }
+
+
+  }
+}
+
+void resize(int width, int height)
+{
+    glViewport(0, 0, width, height);
+
+    glMatrixMode(GL_PROJECTION);//matriz de proyeccion
+    glLoadIdentity();//cargamos la matriz identidad
+    glOrtho(0,600,0,600,-300,300);
+    glMatrixMode(GL_MODELVIEW);//matriz de modelado
+    glLoadIdentity();//matriz identidad
+    glEnable(GL_DEPTH_TEST);//activamos el Test de profundidad
+    glDepthFunc(GL_LEQUAL);//funcion de borrrado buffer depth
+
+}
+void nodos()
+{
+    int x;
+    int y;
+    Point* point;
+    Point* pointRec;
+    glPointSize(10);
+    glBegin(GL_POINTS);
+        for(int i = 0 ; i < graph.getNodesList().size() ;i++){
+            point = &(graph.getNodesList())[i]->m_data;
+            glColor3f(1,1,1);
+            glVertex3f((*point).getX()*REDIM,(*point).getY()*REDIM,0);
+        }
+        for(int i = 0; i < ans.size();i++){
+            pointRec = &ans[i];
+            glColor3f(0,0,1);
+            glVertex3f((*pointRec).getX()*REDIM,(*pointRec).getY()*REDIM,0);
+        }
+    glEnd();
+
+}
+
+void edges()
+{
+    int x;
+    int y;
+    int i , j;
+    Point* point;
+    Point* point2;
+    glBegin(GL_LINES);
+        for(i = 0 ; i < graph.getNodesList().size() ;i++){
+            point = &(graph.getNodesList())[i]->m_data;
+            for ( j = 0 ; j <  ((graph.getNodesList())[i]->getEdges().size()) ;j++){
+                point2 = &(graph.getNodesList())[i]->getEdges()[j]->m_node[1]->m_data;
+                glColor3d(255,0,0);
+                glVertex2d((*point).getX()*REDIM,(*point).getY()*REDIM);
+                glVertex2d((*point2).getX()*REDIM,(*point2).getY()*REDIM);
+
+               // cout<< "("<<(*point).getX()*10 << " , "<< (*point).getY()*10<<")"<< "("<<(*point2).getX()*10 << " , "<< (*point2).getY()*10<<")"<<endl;
+            }
+        }
+        //cout<<i <<" "<<j;
+    glEnd();
+
+
+}
+
+void display()
+{
+     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+     glLoadIdentity();
+     //gluLookAt(1.0,1.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0);
+     nodos();
+     edges();
+     glutSwapBuffers();
+}
+
+
+
+void key(unsigned char key, int x, int y)    //funcion para uso del teclado
+{
+    switch(key)
+    {
+          case 27 :
+            exit(0);
+            break;
+        default:
+            break;
+    }
+
+glutPostRedisplay();    //redibuja el display
+}
+
+void idle()
+{
+    glutPostRedisplay();//redibuja el display
+}
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+    /*cout << "GRAPH NODES" << endl;
+	graph.printNodes();
+	cout << "GRAPH EDGES" << endl;
+	graph.printEdges();
+	cout << "Insert Nodes to find path" << endl;
+	int a,b,c,d;
+    //1 double distance = 0f;
+	cin >> a >> b >> c >> d;
+	Point A,B;
+	A.setX(a);A.setY(b);
+	B.setX(c);B.setY(d);
+
+	float distance = graph.pathBetweenNodes(A,B,ans);
+	for(auto i = static_cast<int>(ans.size() - 1); i >= 0; --i ){
+		cout << ans[i].getX() << ", " << ans[i].getY() << " -> ";
+	}
+	cout << "//" << endl;
+    cout << "COSTO DEL RECORRIDO: " << distance << endl;
+
+*/
+    glutInit(&argc, argv);//inicializacion del glut
+    glutInitWindowSize(600,600);//tamaño de la ventana
+    glutInitWindowPosition(400,100);//posicion de la ventana en el monitor
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);//modo de visualizacion inicial
+    glutCreateWindow("Malla grafo");//nombre de la ventana
+    glClearColor(0,0,0,0);//especificar valores claros para el color del buffer
+    glutReshapeFunc(resize);//control de ventanas
+    glutDisplayFunc(display);//establece la devolución de llamada de visualización de la ventana actual
+    glutKeyboardFunc(key);//funcion teclado especial
+    //glutFullScreen();
+    glutMouseFunc(&OnMouseClick);
+    glutIdleFunc(idle);//funcion de animacion
+
+   // char soundfile[20] ="C:\planeta.wav";//sonido
+    //PlaySound((LPCSTR)soundfile, NULL, SND_FILENAME | SND_ASYNC );//sonido
+
+    glutMainLoop();//bucle de procesamiento de eventos de GLUT
+
+    return 0;
+}
---
+++ ocr-perceptron/perceptron.py
@@ -0,0 +1,130 @@
+import tornado.ioloop
+import tornado.web
+import csv
+import json
+
+# Perceptron Config TODO: obtain config from file
+threshold = 0.5
+learning_rate = 0.1
+trained_perceptrons = {}
+verbose = True
+
+# Helper method to find if a csv row is empty
+def is_row_empty(row):
+    for i in row:
+        if i != '':
+            return False
+    return True
+
+# Reads a csv file and returns a dict with the class name as the key and the values
+# as a 1-dimension vector (list)
+# The csv has the following structure:
+# 'class_name', input_1, input_2, ..., input_n
+#           '', input_1, input_2, ..., input_n
+def process_data(file = 'training_data.csv'):
+    with open(file, 'r') as f:
+        reader = csv.reader(f)
+        data = {}
+        currentClass = ''
+        for row in reader:
+            if is_row_empty(row):
+                pass
+            else:
+                if row[0] != '': #new class
+                    currentClass = row[0]
+                    data[currentClass] = []
+                elements = map(lambda x: int(x), row[1:])    
+                data[currentClass].extend(elements)
+    return data
+
+# Returns the dot product between a collection of values and their weights
+def dot_product(values, weights):
+    return sum(value * weight for value, weight in zip(values, weights))
+
+# Trains the perceptron using the given training_set
+# The training set must be a list of tuples containing (example, desired_output)
+def train(training_set):
+    weights = [0] * len(training_set[0][0])
+    while True:
+        error_count = 0
+        for input_vector, desired_output in training_set:
+            result = dot_product(input_vector, weights) > threshold
+            error = desired_output - result
+            if error != 0:
+                error_count += 1
+                for index, value in enumerate(input_vector):
+                    weights[index] += learning_rate * error * value
+        if error_count == 0:
+            break
+    return weights
+
+# Creates the training set for the given tag
+# Returns a list of tuples (one tuple of (sensor_data, desired_output) for each class)
+def create_training_set(tag, training_data):
+    training_set = []
+    for key, data in training_data.iteritems():
+        training_set.append((tuple(data),1 if key == tag else 0))
+    return training_set
+
+# Runs recognize() on every trained perceptron and returns the first tag that results True
+def classify(sensor_data, perceptrons):
+    ratings = []
+    for key, value in perceptrons.iteritems():
+        verbose("Trying {0}".format(key))
+        ratings.append((key,recognize(sensor_data,value)))
+    verbose("-"*10)
+    return max(ratings, key = lambda i: i[1])[0]
+
+# Tries to recognize sensor_data through a single perceptron. It uses the threshold parameter
+# to determine when it gets fired
+def recognize(sensor_data, weights):
+    result = dot_product(sensor_data, weights)
+    verbose("output: {0} threshold: {1}".format(result, threshold))
+    return result 
+
+
+# Creates and begins perceptron training
+def create_perceptrons():
+    data = process_data()
+    for tag in data.iterkeys():
+        trained_perceptrons[tag] = train(create_training_set(tag, data))
+
+def verbose(s):
+    if verbose:
+        print (s)
+
+def init():
+    print ("***Starting server***")
+    print ("Training perceptrons...")
+    create_perceptrons()
+    print ("Perceptrons trained!")
+
+
+# Small server to listen for /recognize requests at port :8000
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('index.html')
+        
+class RecognizeHandler(tornado.web.RequestHandler):
+    def post(self):
+        sensor_data = json.loads(self.request.body)['sensor']
+        result = {'result':classify(sensor_data, trained_perceptrons)}
+        self.write(result)
+
+def make_app():
+    return tornado.web.Application([
+        (r"/recognize", RecognizeHandler),
+        (r"/", MainHandler),
+        (r"/(.*)", 
+            tornado.web.StaticFileHandler,
+            {"path":r"web/"})
+    ])
+
+if __name__ == "__main__":
+    init()
+    app = make_app()
+    app.listen(8000)
+    print ("Listening to port :8000")
+    tornado.ioloop.IOLoop.current().start()
+        
